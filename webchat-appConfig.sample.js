15bbe479fc7da6fd0ddb2b317043bb108ac09212
TerinCas <78723061+TerinCas@users.noreply.github.com>
2021-02-08 13:52:10 +0530

    Add files via upload

---
+++ webchat-appConfig.sample.js
@@ -0,0 +1,9 @@
+var appConfig = {
+    //  change the your AccountSid
+    accountSid: "ACdbb.....",
+    // change to your Flex Flow SID
+    flexFlowSid: "FO1bfb...",
+    colorTheme: {
+        overrides: brandedColors
+    }
+}
